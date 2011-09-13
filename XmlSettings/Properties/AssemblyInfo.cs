9d5da56042ed00adad89213bb97d986dbb958d96
David Fowler <davidfowl@gmail.com>
2011-09-13 13:37:47 -0700

    Added nuspec file

--- XmlSettings/Properties/AssemblyInfo.cs
+++ XmlSettings/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("XmlSettings")]
-[assembly: AssemblyDescription("A general purpose settings ini style file in xml format.")]
+[assembly: AssemblyDescription("A general purpose settings .ini style file in xml format.")]
 [assembly: AssemblyCompany("David Fowler")]
 [assembly: AssemblyProduct("XmlSettings")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
