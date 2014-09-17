2d8c399d2714adfe7f4a15f3cadd8985ccff7d4b
David Fowler <davidfowl@gmail.com>
2014-09-17 02:08:59 -0700

    Change to use new ASP.NET vNext project system

--- XmlSettings/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("XmlSettings")]
-[assembly: AssemblyDescription("A general purpose settings .ini style file in xml format.")]
-[assembly: AssemblyCompany("David Fowler")]
-[assembly: AssemblyProduct("XmlSettings")]
-
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
